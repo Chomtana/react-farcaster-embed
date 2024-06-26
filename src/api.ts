4b75e38e76e4807f0591487d65c47f2e5384e796
Chomtana <Chomtana001@gmail.com>
2024-06-26 15:08:59 +0700

    Fix get cast bug

--- src/api.ts
+++ src/api.ts
@@ -7,23 +7,8 @@ export const getCast = async (username: string, hash: string, options?: { custom
     );
     const cast = await res.json();
 
-    // Handle weird reply to a cast in a channel returning the original cast.
-    // We need to check if the username and hash are matching the linked cast to render the right one.
-    // Very weird API, but it's the only way to get the right cast.
-    if (
-      cast.result.casts[2] &&
-      cast.result.casts[2].author.username === username &&
-      cast.result.casts[2].hash.includes(hash)
-    ) {
-      return cast.result.casts[2];
-    }
-
-    // Handle skipping root-embed casts which are empty parents for a cast in a channel.
-    if (cast.result.casts[0].castType === "root-embed") {
-      return cast.result.casts[1];
-    }
-
-    return cast.result.casts[0];
+    // Find a cast with a matching hash or return the first cast if the matched cast is not found
+    return cast.result.casts.find((c: any) => c.hash.includes(hash)) ?? cast.result.casts[0];
   } catch (e) {
     console.error(e);
     throw new Error(
